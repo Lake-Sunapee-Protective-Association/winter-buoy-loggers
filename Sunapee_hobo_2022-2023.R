65ecdc77a38da2855b13eb98d93fdd9a9c86626e
steeleb <steeleb@caryinstitute.org>
2023-08-20 17:05:01 -0600

    update winter 22-23 code

--- Sunapee_hobo_2022-2023.R
+++ Sunapee_hobo_2022-2023.R
@@ -11,7 +11,7 @@ library(tidyverse)
 library(lubridate)
 
 # point to directories
-data_dir = 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L0/winter hobo data/2023-2023WinterTemp_U22/'
+data_dir = 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L0/winter hobo data/2022-2023WinterTemp/'
 dump_dir = 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L1/winter/'
 
 # save functions/groms
@@ -21,6 +21,10 @@ final_theme=theme_bw() +
         plot.title=element_text(size=16, face='bold', hjust=0.5)) #save as a grom
 
 #bring in winter 21-22 hobo raw data
+hobo_w22_1m <- read.csv(file.path(data_dir, 'U22-1m.csv'),
+                        col.names = c('obs', 'datetime', 'TempC_1m', 'detach', 'attach', 'connect', 'stop', 'eof'),
+                        skip =2) %>% 
+  select(-obs, -c(detach:eof))
 hobo_w22_2m <- read.csv(file.path(data_dir, 'U22-2m.csv'),
                         col.names = c('obs', 'datetime', 'TempC_2m', 'detach', 'attach', 'connect', 'stop', 'eof'),
                         skip =2) %>% 
@@ -37,7 +41,10 @@ hobo_w22_5m <- read.csv(file.path(data_dir, 'U22-5m.csv'),
                         col.names = c('obs', 'datetime', 'TempC_5m', 'detach', 'attach', 'connect', 'stop', 'eof'),
                         skip =2) %>% 
   select(-obs, -c(detach:eof))
-#no 6m data
+hobo_w22_6m <- read.csv(file.path(data_dir, 'U22-6m.csv'),
+                        col.names = c('obs', 'datetime', 'TempC_6m', 'detach', 'attach', 'connect', 'stop', 'eof'),
+                        skip =2) %>% 
+  select(-obs, -c(detach:eof))
 hobo_w22_7m <- read.csv(file.path(data_dir, 'U22-7m.csv'),
                         col.names = c('obs', 'datetime', 'TempC_7m', 'detach', 'attach', 'connect', 'stop', 'eof'),
                         skip =2) %>% 
@@ -55,16 +62,18 @@ hobo_w22_10m <- read.csv(file.path(data_dir, 'U22-10m.csv'),
                          skip =2) %>% 
   select(-obs, -c(detach:eof))
 
-#no 1m DO deployed this year
-hobo_w22 <- full_join(hobo_w22_2m, hobo_w22_3m) %>% 
+# join together
+hobo_w22 <- full_join(hobo_w22_1m, hobo_w22_2m) %>% 
+  full_join(., hobo_w22_3m) %>% 
   full_join(., hobo_w22_4m) %>% 
   full_join(., hobo_w22_5m) %>% 
   full_join(., hobo_w22_7m) %>% 
+  full_join(., hobo_w22_6m) %>% 
   full_join(., hobo_w22_8m) %>% 
   full_join(., hobo_w22_9m) %>% 
   full_join(., hobo_w22_10m)
 
-rm(hobo_w22_2m, hobo_w22_3m, hobo_w22_4m, hobo_w22_5m, hobo_w22_7m, hobo_w22_8m, hobo_w22_9m, hobo_w22_10m)
+rm(hobo_w22_1m, hobo_w22_2m, hobo_w22_3m, hobo_w22_4m, hobo_w22_5m, hobo_w22_6m, hobo_w22_7m, hobo_w22_8m, hobo_w22_9m, hobo_w22_10m)
 
 #format date of hobo sensors
 hobo_w22 <- hobo_w22 %>% 
@@ -147,26 +156,34 @@ ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-04-01' & datetime < '20
   scale_x_datetime(date_minor_breaks = '1 day') +
   final_theme
 
-#removal 2023-04-26
-
-ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-04-26' & datetime < '2023-04-27')), aes(x=datetime, y=value, col=variable)) +
+ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-05-01' & datetime < '2023-06-01')), aes(x=datetime, y=value, col=variable)) +
   geom_point() +
-  labs(title='hobo temp June 2023', x='date', y='temp (deg C)') +
+  labs(title='hobo temp MAy 2023', x='date', y='temp (deg C)') +
+  scale_color_manual(values=c("#000000", "#999999", "#997300", "#ffbf00", "#173fb5", "#a5b8f3", "#004d13",
+                              "#00e639", "#d4c711", "#0081cc", "#66c7ff")) +
+  scale_x_datetime(date_minor_breaks = '1 day') +
+  final_theme
+
+# loggers removed May 15
+ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-05-15' & datetime < '2023-06-15')), aes(x=datetime, y=value, col=variable)) +
+  geom_point() +
+  labs(title='hobo temp MAy 2023', x='date', y='temp (deg C)') +
   scale_color_manual(values=c("#000000", "#999999", "#997300", "#ffbf00", "#173fb5", "#a5b8f3", "#004d13",
                               "#00e639", "#d4c711", "#0081cc", "#66c7ff")) +
   scale_x_datetime(date_minor_breaks = '1 hour') +
   final_theme
 
+
 hobo_w22_L1 <- hobo_w22_L1 %>% 
-  mutate_at(vars("TempC_2m", "TempC_3m", "TempC_4m", "TempC_5m", "TempC_7m", "TempC_8m", "TempC_9m", "TempC_10m"),
-            ~(case_when(datetime >= as.POSIXct('2023-04-26 11:30', tz='UTC') ~ NA_real_,
+  mutate_at(vars("TempC_1m", "TempC_2m", "TempC_3m", "TempC_4m", "TempC_5m", "TempC_6m", "TempC_7m", "TempC_8m", "TempC_9m", "TempC_10m"),
+            ~(case_when(datetime >= as.POSIXct('2023-05-15 11:00', tz='UTC') ~ NA_real_,
                            TRUE ~ .)))
 hobo_temp_vert_L1 <- hobo_w22_L1 %>%
   gather(variable, value, -datetime)
 
-ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-04-01' & datetime < '2023-05-01')), aes(x=datetime, y=value, col=variable)) +
+ggplot(subset(hobo_temp_vert_L1, subset=(datetime>='2023-05-01' & datetime < '2023-06-01')), aes(x=datetime, y=value, col=variable)) +
   geom_point() +
-  labs(title='hobo temp Apr 2023', x='date', y='temp (deg C)') +
+  labs(title='hobo temp May 2023', x='date', y='temp (deg C)') +
   scale_color_manual(values=c("#000000", "#999999", "#997300", "#ffbf00", "#173fb5", "#a5b8f3", "#004d13",
                               "#00e639", "#d4c711", "#0081cc", "#66c7ff")) +
   scale_x_datetime(date_minor_breaks = '1 day') +
@@ -180,9 +197,6 @@ ggplot(hobo_temp_vert_L1, aes(x=datetime, y=value, col=variable)) +
   scale_x_datetime(date_minor_breaks = '1 month') +
   final_theme
 
-
-#### do unit ####
-# no do this year.
   
 # data stored in GMT-4, change to GMT-5
 hobo_w22_L1 <- hobo_w22_L1 %>% 
@@ -190,11 +204,13 @@ hobo_w22_L1 <- hobo_w22_L1 %>%
   
 #export L1 tempstring file
 hobo_w22_L1 %>% 
-  select(datetime, TempC_2m, TempC_3m, TempC_4m, TempC_5m, TempC_7m, TempC_8m, TempC_9m, TempC_10m) %>% 
-  rename(waterTemperature_degC_2m = 'TempC_2m',
+  select(datetime, TempC_1m, TempC_2m, TempC_3m, TempC_4m, TempC_5m, TempC_6m, TempC_7m, TempC_8m, TempC_9m, TempC_10m) %>% 
+  rename(waterTemperature_degC_1m = 'TempC_1m',
+         waterTemperature_degC_2m = 'TempC_2m',
          waterTemperature_degC_3m = 'TempC_3m',
          waterTemperature_degC_4m = 'TempC_4m',
          waterTemperature_degC_5m = 'TempC_5m',
+         waterTemperature_degC_6m = 'TempC_6m',
          waterTemperature_degC_7m = 'TempC_7m',
          waterTemperature_degC_8m = 'TempC_8m',
          waterTemperature_degC_9m = 'TempC_9m',
